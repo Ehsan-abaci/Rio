9b7b31dec7e3ff5b3183e6e13635f272581cf606
unknown <farzanemojtabaviofficial@gmail.com>
2024-05-23 23:46:37 +0330

    replace pushAndRemoveUntil to  pushReplacement in splash screen page

--- lib/feature/splash/presentation/screens/splash_screen_page.dart
+++ lib/feature/splash/presentation/screens/splash_screen_page.dart
@@ -3,37 +3,45 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../../../../core/utils/resources/assets_manager.dart';
 import '../../../signup/presentation/screens/signup_page.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
     Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => const SignupPage()),
-        (Route<dynamic> route) => false,
-      );
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (BuildContext context) => const SignupPage()));
     });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: SvgPicture.asset(
-              AssetsImage.bg,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Center(
-            child: Positioned.fill(
-              child: SvgPicture.asset(
-                AssetsImage.logo,
-                fit: BoxFit.fill,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  AssetsImage.bg,
+                  fit: BoxFit.fill,
+                ),
               ),
-            ),
+              Center(
+                child: Positioned.fill(
+                  child: SvgPicture.asset(
+                    AssetsImage.logo,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
